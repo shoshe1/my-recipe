a698abf4b70aaa54b9d2dce0147fa7dc55d7c3e9
shoshe1 <shahdbaloum2006@gmail.com>
2025-12-13 15:18:39 +0200

    implement React Router with all application routes

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -1,40 +1,31 @@
-import { useState } from 'react';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import FormPage from './pages/FormPage';
 import ApiPage from './pages/ApiPage';
+import FavoritesPage from './pages/FavoritesPage';
+import { FavoritesProvider } from './context/favoritesContext';
+import NotFoundPage from './pages/NotFoundPage';
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
-
-
-
 function App() {
-    const [currentPage , setCurrentPage]= useState('home');
-
-
-    const renderPage= () => {
-        switch (currentPage) {
-            case  'home':
-                return <HomePage/>;
-            case 'form':
-                return <FormPage/>;
-            case 'api':
-                return<ApiPage/>;
-            default:
-                return <HomePage/>;
-            
-        
-        }
-    };
-    
     return (
-        <div className="app">
-            <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
-            <main className="main-content">
-                {renderPage()}
-            </main>
+        <FavoritesProvider>
+            <div className='app'>
+                <Header />
+                <main className='main-content'>
+                    <Routes>
+                        <Route path='/' element={<HomePage />} />
+                        <Route path='/form' element={<FormPage />} />
+                        <Route path='/api' element={<ApiPage />} />
+                        <Route path='/favorites' element={<FavoritesPage />} />
+                        <Route path='*' element={<NotFoundPage />} />
+                    </Routes>
+                </main>
             </div>
+        </FavoritesProvider>
     );
 }
+
 export default App;
