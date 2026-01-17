15cb67b07da12abd19c9d4810d7928fab834b3c2
shoshe1 <shahdbaloum2006@gmail.com>
2026-01-17 16:07:28 +0200

    Wrap app with Redux Provider and React Router

--- recipe-app/src/index.js
+++ recipe-app/src/index.js
@@ -4,11 +4,17 @@ import './App.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
