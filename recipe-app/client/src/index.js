0193c22123cf1b4c7da7afed35d543ae466fefb6
shoshe1 <shahdbaloum2006@gmail.com>
2026-01-29 23:09:47 +0200

    integrate AuthProvider for authentication context
    Add AuthProvider wrapper to provide auth state globally throughout the app.

--- recipe-app/client/src/index.js
+++ recipe-app/client/src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
